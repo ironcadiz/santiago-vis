c23e86a50389319eec8051ba02db3ddec894fab5
ironcadiz <afcadiz@uc.cl>
2019-11-20 22:38:22 -0300

    added about content

--- map.js
+++ map.js
@@ -1,7 +1,14 @@
 var width = 1920
 var height = 1280
 var centered;
-var sampleSize = 5000;
+var isMobile = false; //initiate as false
+// device detection
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+    isMobile = true;
+}
+var sampleSize = isMobile? 1000 : 5000;
+var radius = isMobile?  7:4;
 
 // md components setup
 const select = new mdc.select.MDCSelect(document.querySelector('.mdc-select'));
@@ -59,7 +66,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZW5qYWxvdCIsImEiOiJjaWhtdmxhNTIwb25zdHBsejk0N
 //Setup mapbox-gl map
 var map = new mapboxgl.Map({
   container: 'map', // container id
-  style: 'mapbox://styles/mapbox/streets-v11',
+  style: 'mapbox://styles/mapbox/dark-v10',
   center: [-70.670000,-33.509502],
   zoom: 11,
 
@@ -178,7 +185,7 @@ d3.csv("data/santiago_rank.csv", function(data) {
       .attr("fill",d => colorScales[cat](d[cat]))
       .on("click",click_dot)
       .transition().duration(1000)
-      .attr("r", 4)
+      .attr("r", radius)
 
   buttonSample.listen('click', () => {
         sampleSize = Math.max(Math.min(Number(textField.value),80000),0)
@@ -208,7 +215,7 @@ d3.csv("data/santiago_rank.csv", function(data) {
             .attr("fill",d => colorScales[cat](d[cat]))
             .on("click",click_dot)
             .transition().duration(1000)
-            .attr("r", 4)
+            .attr("r", radius)
         dots.exit().transition().duration(1000).attr("r", 0).remove()
       })
 
@@ -228,7 +235,7 @@ d3.csv("data/santiago_rank.csv", function(data) {
 
 
 // add the legend now
-var legend_w = 500, legend_h = 70;
+var legend_w = "50vw", legend_h = 70;
 
 lColors = legendColors(cat)
 var key = mapLayer
@@ -236,8 +243,8 @@ var key = mapLayer
   .attr("width", legend_w)
   .attr("height", legend_h)
   .classed("legend",true)
-  .attr("x",width/2 - legend_w/2)
-  .attr("y",900)
+  .attr("x","25vw")
+  .attr("y","80vh")
 
 var legend = key.append("defs")
   .append("svg:linearGradient")
